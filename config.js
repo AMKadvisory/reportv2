e33c667d8c72f15abbd038b0a42f8af0d302376f
AMKadvisory <n.rahat@amkadvisory.com>
2026-04-23 12:44:54 +0600

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 //  Requires: PocketBase JS SDK loaded globally
 // ═══════════════════════════════════════════════════════════
 const POCKETBASE_URL = 'http://127.0.0.1:8090';
-const db = new PocketBase(POCKETBASE_URL);
+const db = (typeof _db !== 'undefined') ? _db : new PocketBase(POCKETBASE_URL);
